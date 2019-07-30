75fcaed842759c5a42fc4e4e80c36ebd57a5d179
viliusgudziunas <vilius.gudziunas@yahoo.com>
2019-07-30 10:42:10 +0000

    Develop

--- src/components/Goal.js
+++ src/components/Goal.js
@@ -1,21 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './styles/Goal.css';
-import { Row, Col, Button } from 'react-bootstrap';
+import { Row, Col, Button, Accordion, Card } from 'react-bootstrap';
+import GoalOptions from './GoalOptions';
 
-const Goal = ({ name }) => {
+const Goal = ({ name, eventKey, onDeleteGoal }) => {
   return (
-    <Row className='goal-row'>
-      <Col>{name}</Col>
-      <Button variant='success' size='sm' className='goal-done-button'>
-        Done!
-      </Button>
-    </Row>
+    <Card>
+      <Card.Header className='goal-card-header'>
+        <Row>
+          <Accordion.Toggle
+            as={Col}
+            eventKey={eventKey}
+            className='goal-card-accordion-toggle'
+          >
+            {name}
+          </Accordion.Toggle>
+          <Button variant='success' size='sm' className='goal-done-button'>
+            Done!
+          </Button>
+        </Row>
+      </Card.Header>
+      <Accordion.Collapse eventKey={eventKey}>
+        <GoalOptions name={name} onDeleteGoal={onDeleteGoal} />
+      </Accordion.Collapse>
+    </Card>
   );
 };
 
 export default Goal;
 
 Goal.propTypes = {
-  name: PropTypes.string
+  name: PropTypes.string,
+  eventKey: PropTypes.number,
+  onDeleteGoal: PropTypes.func
 };
