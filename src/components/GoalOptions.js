467a872e3f2da2931ce891f23408a9cf5b22be85
Vilius Gudziunas <vilius.gudziunas@landregistry.gov.uk>
2019-07-30 15:54:09 +0100

    Delete Goal button now gets disabled straight after it is clicked so two goals don't get accidentaly deleted

--- src/components/GoalOptions.js
+++ src/components/GoalOptions.js
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './styles/GoalOptions.css';
 import { Card, Button } from 'react-bootstrap';
 
 const GoalOptions = ({ name, onDeleteGoal }) => {
+  const [disableDeleteButton, setDisableDeleteButton] = useState(false);
+
   const handleDeleteGoal = async () => {
+    setDisableDeleteButton(true);
     const response = await fetch(`/goals/delete-goal/${name}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       }
     });
-    onDeleteGoal(name);
+    if (response.ok) {
+      onDeleteGoal(name);
+    }
+    setDisableDeleteButton(false);
   };
 
   return (
     <Card.Body>
-      <Button className='goalOptions-delete-button' onClick={handleDeleteGoal}>
+      <Button
+        className='goalOptions-delete-button'
+        onClick={handleDeleteGoal}
+        disabled={disableDeleteButton}
+      >
         Delete Goal
       </Button>
     </Card.Body>
