78da769d0e82210cea99cbc38c4adfbb2d143200
viliusgudziunas <vilius.gudziunas@yahoo.com>
2019-08-06 20:43:17 +0000

    Delete Goal button now gets disabled straight after it is clicked so two goals don't get accidentaly deleted

--- src/components/GoalOptions.js
+++ src/components/GoalOptions.js
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './styles/GoalOptions.css';
-import { Card, Button } from 'react-bootstrap';
+import { Container, Card, Button } from 'react-bootstrap';
 
-const GoalOptions = ({ name, onDeleteGoal }) => {
+const GoalOptions = ({ name, target, onDeleteGoal }) => {
+  const [disableDeleteButton, setDisableDeleteButton] = useState(false);
+
   const handleDeleteGoal = async () => {
+    setDisableDeleteButton(true);
     const response = await fetch(`/goals/delete-goal/${name}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       }
     });
-    onDeleteGoal(name);
+    if (response.ok) {
+      onDeleteGoal(name);
+    }
+    setDisableDeleteButton(false);
   };
 
   return (
     <Card.Body>
-      <Button className='goalOptions-delete-button' onClick={handleDeleteGoal}>
-        Delete Goal
-      </Button>
+      <Container className='goalOptions-container1'>
+        Montly target - {target}
+      </Container>
+      <Container className='goalOptions-container2'>
+        <Button
+          className='goalOptions-delete-button'
+          onClick={handleDeleteGoal}
+          disabled={disableDeleteButton}
+        >
+          Delete Goal
+        </Button>
+      </Container>
     </Card.Body>
   );
 };
@@ -26,6 +41,7 @@ const GoalOptions = ({ name, onDeleteGoal }) => {
 export default GoalOptions;
 
 GoalOptions.propTypes = {
-  name: PropTypes.string,
-  onDeleteGoal: PropTypes.func
+  name: PropTypes.string.isRequired,
+  target: PropTypes.number.isRequired,
+  onDeleteGoal: PropTypes.func.isRequired
 };
--- src/components/Goals.js
+++ src/components/Goals.js
@@ -4,17 +4,18 @@ import './styles/Goals.css';
 import { Container, Accordion } from 'react-bootstrap';
 import Goal from './Goal';
 
-const Goals = ({ goals, onDeleteGoal }) => {
+const Goals = ({ goals, onDeleteGoal, onCompleteGoal }) => {
   return (
     <Container>
       <Accordion className='goals-accordion'>
         {goals.map((goal, index) => {
           return (
             <Goal
-              key={goal.name}
-              name={goal.name}
+              key={goal.id}
+              goal={goal}
               eventKey={index}
               onDeleteGoal={onDeleteGoal}
+              onCompleteGoal={onCompleteGoal}
             />
           );
         })}
@@ -26,6 +27,21 @@ const Goals = ({ goals, onDeleteGoal }) => {
 export default Goals;
 
 Goals.propTypes = {
-  goals: PropTypes.array,
-  onDeleteGoal: PropTypes.func
+  goals: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      target: PropTypes.number.isRequired,
+      timestamp: PropTypes.string.isRequired,
+      instances: PropTypes.arrayOf(
+        PropTypes.shape({
+          id: PropTypes.number.isRequired,
+          goal_id: PropTypes.number.isRequired,
+          timestamp: PropTypes.string.isRequired
+        })
+      ).isRequired
+    })
+  ).isRequired,
+  onDeleteGoal: PropTypes.func.isRequired,
+  onCompleteGoal: PropTypes.func.isRequired
 };
