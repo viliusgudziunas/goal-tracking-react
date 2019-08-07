aa0b70424a6f001675de913850479994a214f388
Vilius Gudziunas <vilius.gudziunas@yahoo.com>
2019-08-07 22:59:59 +0100

    WIP: Working on the form to change goal target

--- src/components/GoalOptions.js
+++ src/components/GoalOptions.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './styles/GoalOptions.css';
-import { Container, Card, Button, Row } from 'react-bootstrap';
+import { Container, Card, Button, Row, Form, Col } from 'react-bootstrap';
 
 const GoalOptions = ({ goal, onDeleteGoal, onChangeTarget }) => {
   const [disableDeleteButton, setDisableDeleteButton] = useState(false);
@@ -9,6 +9,8 @@ const GoalOptions = ({ goal, onDeleteGoal, onChangeTarget }) => {
     false
   );
   const [completedGoalsThisWeek, setCompletedGoalsThisWeek] = useState(0);
+  const [displayChangeTargetForm, setDisplayChangeTargetForm] = useState(false);
+  const [newGoalTarget, setNewGoalTarget] = useState('');
 
   const countInstancesThisWeek = timestamp => {
     const lastMonday = new Date();
@@ -41,22 +43,45 @@ const GoalOptions = ({ goal, onDeleteGoal, onChangeTarget }) => {
     setDisableDeleteButton(false);
   };
 
-  const handleChangeTarget = () => {
+  const handleChangeTargetButtonClick = () => {
     setDisableChangeTargetButton(true);
-    const newTarget = { goal_id: goal.id, target: 15 };
-    fetch(`/goals/change-goal-target/${goal.id}`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(newTarget)
-    }).then(res => {
-      res.json().then(response => {
-        onChangeTarget(response);
-      });
-    });
-    setDisableChangeTargetButton(false);
+    setDisplayChangeTargetForm(true);
+    // const newTarget = { goal_id: goal.id, target: 15 };
+    // fetch(`/goals/change-goal-target/${goal.id}`, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   },
+    //   body: JSON.stringify(newTarget)
+    // }).then(res => {
+    //   res.json().then(response => {
+    //     onChangeTarget(response);
+    //   });
+    // });
+    // setDisableChangeTargetButton(false);
   };
+
+  const handleGoalTargetChange = e => {
+    if (e.target.value === '') {
+      setNewGoalTarget(e.target.value);
+      console.log('Goal target empty');
+    } else {
+      if (isNaN(e.target.value)) {
+        console.log('Not a number');
+      } else if (!Number.isInteger(Number(e.target.value))) {
+        console.log('Not an integer');
+      } else if (e.target.value < 1) {
+        console.log('Smaller than 1');
+      } else if (Number(e.target.value) === goal.target) {
+        console.log('Same value as current target');
+      } else {
+        console.log('Goal target valid');
+      }
+      setNewGoalTarget(e.target.value);
+    }
+  };
+
+  const handleChangeTargetFormSubmit = () => {};
 
   return (
     <Card.Body>
@@ -65,26 +90,51 @@ const GoalOptions = ({ goal, onDeleteGoal, onChangeTarget }) => {
           Weekly Progress: {completedGoalsThisWeek}/{goal.target}
         </Row>
       </Container>
-      <Container className='goalOptions-container2'>
-        <Row>
-          <Button
-            className='goalOptions-change-target-button'
-            onClick={handleChangeTarget}
-            disabled={disableChangeTargetButton}
-          >
-            Change Target
-          </Button>
-        </Row>
-        <Row>
-          <Button
-            className='goalOptions-delete-button'
-            onClick={handleDeleteGoal}
-            disabled={disableDeleteButton}
-          >
-            Delete Goal
-          </Button>
-        </Row>
-      </Container>
+      {displayChangeTargetForm && (
+        <Container className='goalOptions-container2'>
+          <Form onSubmit={handleChangeTargetFormSubmit}>
+            <Form.Row>
+              <Col md='auto'>
+                <Form.Control
+                  required
+                  type='goalTarget'
+                  value={newGoalTarget}
+                  onChange={handleGoalTargetChange}
+                  placeholder='Enter New Goal Target'
+                />
+              </Col>
+              <Col md='auto'>
+                <Button type='submit'>Change Target</Button>
+              </Col>
+              <Col md='auto'>
+                <Button variant='secondary'>Back</Button>
+              </Col>
+            </Form.Row>
+          </Form>
+        </Container>
+      )}
+      {!displayChangeTargetForm && (
+        <Container className='goalOptions-container3'>
+          <Row>
+            <Button
+              className='goalOptions-change-target-button'
+              onClick={handleChangeTargetButtonClick}
+              disabled={disableChangeTargetButton}
+            >
+              Change Target
+            </Button>
+          </Row>
+          <Row>
+            <Button
+              className='goalOptions-delete-button'
+              onClick={handleDeleteGoal}
+              disabled={disableDeleteButton}
+            >
+              Delete Goal
+            </Button>
+          </Row>
+        </Container>
+      )}
     </Card.Body>
   );
 };
