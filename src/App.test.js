da22e37455a90b1a7b08b679fe54359fe7ae2779
Vilius Gudziunas <vilius.gudziunas@landregistry.gov.uk>
2019-08-13 15:22:18 +0100

    Revert "WIP: Working on implementing tests into the application"
    This reverts commit d3b65cfed8fd2236f28d081c7e95f1ee77da79fe.

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from './App';
-
-it('renders without crashed', () => {
-  const component = shallow(<App />);
-  expect(component).toMatchSnapshot();
-});
